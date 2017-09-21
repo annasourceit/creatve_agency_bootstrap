a37c9a5ad2ce1e7d7968e8745513b6cbbb7bbbf0
Anna <anna.source.it@gmail.com>
2017-09-21 22:17:56 +0300

    Creative-agency bootstrap about.html

---
+++ assets/src/js/scripts.js
@@ -0,0 +1,13 @@
+;(function ($) {
+    // Scripts that will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute.
+    $(document).ready(function () {
+        var headerHeight = $('.header').outerHeight();
+        var navHeight = $('.header')
+        console.log( headerHeight);
+
+
+
+
+    });
+
+})(jQuery); // Fully reference jQuery after this point.
